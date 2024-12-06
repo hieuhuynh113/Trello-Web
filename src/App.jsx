03bf31fa00f5d10a3c8b4a2b16b4ec65bd425d90
hieuhuynh113 <hieu01645369512@gmail.com>
2024-12-06 18:15:48 +0700

    Import light, dark mode and Update theming

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
-
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>Hieu Huynh</div>
 
       <Typography variant="body2" color="text.secondary">Test Typography</Typography>
@@ -16,7 +32,7 @@ function App() {
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
       <br />
-      
+
       <HomeIcon />
       <HomeIcon color="primary" />
       <HomeIcon color="secondary" />
