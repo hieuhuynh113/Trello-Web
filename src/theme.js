9e7a5063cb739f33b555db7044fe43923e289d09
hieuhuynh113 <hieu01645369512@gmail.com>
2024-12-08 22:53:48 +0700

    Update Color Layout

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,3 @@
-import { cyan, deepOrange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create theme instance
@@ -10,18 +9,18 @@ const theme = extendTheme({
   },
   // Color schemes used in theme
   colorSchemes: {
-    light: {
-      palette: {
-        primary: teal, // Primary color, used for buttons, inputs, ...
-        secondary: deepOrange // Secondary color, used for buttons, inputs, ...
-      }
-    },
-    dark: {
-      palette: {
-        primary: cyan, // Primary color, used for buttons, inputs, ...
-        secondary: deepOrange // Secondary color, used for buttons, inputs, ...
-      }
-    }
+    // light: {
+    //   palette: {
+    //     primary: teal, // Primary color, used for buttons, inputs, ...
+    //     secondary: deepOrange // Secondary color, used for buttons, inputs, ...
+    //   }
+    // },
+    // dark: {
+    //   palette: {
+    //     primary: cyan, // Primary color, used for buttons, inputs, ...
+    //     secondary: deepOrange // Secondary color, used for buttons, inputs, ...
+    //   }
+    // }
   },
   // MUI components, use style overrides
   components: {
@@ -34,11 +33,11 @@ const theme = extendTheme({
             height: '8px' // Scrollbar height
           },
           '*::-webkit-scrollbar-thumb': {
-            backgroundColor: '#bdc3c7', // Scrollbar thumb color
+            backgroundColor: '#dcdde1', // Scrollbar thumb color
             borderRadius: '8px' // Scrollbar thumb border radius
           },
           '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#00b894' // Scrollbar thumb color on hover
+            backgroundColor: 'white' // Scrollbar thumb color on hover
           }
         }
       }
@@ -54,25 +53,18 @@ const theme = extendTheme({
     // InputLabel, use style overrides
     MuiInputLabel: {
       styleOverrides: {
-        root: ({ theme }) => ({
-          color: theme.palette.primary.main, // Label color
-          fontSize: '0.875rem' // Label font size
-        })
+        root: { fontSize: '0.875rem' } // Label font size
       }
     },
     // OutlinedInput, use style overrides
     MuiOutlinedInput: {
       styleOverrides: {
-        root: ({ theme }) => ({
-          color: theme.palette.primary.main, // Input color
+        root: {
           fontSize: '0.875rem', // Input font size
-          '.MuiOutlinedInput-notchedOutline': {
-            borderColor: theme.palette.primary.light // Input border color
-          },
-          '&:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: theme.palette.primary.main // Input border color on hover
-          }
-        })
+          '& fieldset': { borderWidth: '0.5px !important' },
+          '&:hover fieldset': { borderWidth: '1.6px !important' },
+          '&:focus fieldset': { borderWidth: '1.6px !important' }
+        }
       }
     }
   }
